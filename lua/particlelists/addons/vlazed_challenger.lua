8d15f9548935d1c3edd2385d9597435d7f5fcbbe
vlazed <dinoboy.valdez4@gmail.com>
2025-10-28 12:42:49 -0500

    Add small aura particle fx + Aura FX with color support through Advanced Particle Controller

---
+++ lua/particlelists/addons/vlazed_challenger.lua
@@ -0,0 +1,30 @@
+"EffectList"
+{
+
+
+
+	"Info"
+	{
+		"CategoryName" "Zhang Wei"
+	
+		"EffectOptions"
+		{
+			"Color1"
+			"
+			aura_erratic
+			aura_erratic2
+			"
+		}
+
+	}
+
+
+
+"vlazed/challenger.pcf"
+"
+aura_erratic
+aura_erratic2
+"
+
+
+}
